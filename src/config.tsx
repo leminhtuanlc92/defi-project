24e264c88b942134e762facae79085fb00f8e544
Louis <congnt@hvtc.edu.vn>
2020-06-08 08:35:17 +0700

    edit contract

--- src/config.tsx
+++ src/config.tsx
@@ -9,8 +9,8 @@ const contract = {
   matrixMarketingAddress: "TBnMmQH2wDfWKwT2Xfdtk34tWkWPZqSnRB",
   matrixMemberAddress: "TWLuojVt13mZT7HgCBvMpVvdKa77kMijnb",
   userDataAddress: "TNwj9mK3YaYhRKZxc3JK9q5PzZuuj82rDw",
-  memberAddress: "TBtcXLVJ3KQfjnj5KpKLDqEy4mXaUjiPTJ",
-  shareHolderAddress: "TERnypgj2vCUTW1fdpnmTY8EeaGraqaXPF",
+  memberAddress: "TJUZY1uN3rZWNNHXJZRidZEGQ3NQCYujCy",
+  shareHolderAddress: "THg9a98T43cgBLCfuxGcKaWxRYSUDKW2LD",
   tokenAddress: "TP2sjjD5Xn2MhNTSBvG1Ygxotwb2nnXJNx",
   usdtAddress: "TG4CbQ3Vjq4oak7qPKxEn8N7ZMzRP77YDF",
 };
