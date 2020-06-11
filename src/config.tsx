1e094b7c926ac8dc5e03db9f3570f930e84a4ab6
Louis <congnt@hvtc.edu.vn>
2020-06-11 08:40:51 +0700

    add favicon & new contract

--- src/config.tsx
+++ src/config.tsx
@@ -19,10 +19,10 @@ const contract = {
   adminAddress: "TTnk8vg7EvKyoic5GcTdpca6rqYwo3GmpW",
   tokenAddress: "TMw6rLM7Ptwti61LkEPimgaDRibefCiwMX",
   usdtAddress: "THriZvGCcQ1sGesZ8EATwGihJk9zqZDi3u",
-  userDataAddress: "TYPV1XXULCmDu3CjRdbQc1gFoope6i9fWh",
-  memberAddress: "TEaKxLZqneX4Q54fsZNoCwSRP3kU3zYpbY",
-  shareHolderAddress: "TDfrTQYfkdvio2swib3dQkcfWaMrip6Sg3",
-  matrixMemberAddress: "TXee5jVnnpqQN3h7i8dzCgyBURVvu7CUTW",
-  matrixMarketingAddress: "TC8jztVWsKiEWcqeLw8E7BxcM7NKDvU537",
+  userDataAddress: "TN3YDDJBdtR2jnhiaBF1AmwGbyX1bByEJR",
+  memberAddress: "TMhFp8em6JJoVLhK2YtvVtHxtCrioqRz2v",
+  shareHolderAddress: "TFwzERbu2u7LdtaqpHn4ce4ZGBbRXaSKhe",
+  matrixMemberAddress: "TX329EuHX7VmhxaKfZnjde89Wv5twFUV9Y",
+  matrixMarketingAddress: "TCxRuF91XFCyVP2waDqi2WYCBri81sKB3b",
 };
 export { config, contract };
